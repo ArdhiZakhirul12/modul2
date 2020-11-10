2426322ac783310fb0be45e23feb2382a37e233f
ArdhiZakhirul12 <53462558+ArdhiZakhirul12@users.noreply.github.com>
2020-11-10 12:49:27 +0700

    Add files via upload

---
+++ modul2/todolist/app.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+    el: "#app",
+    data() {
+        return {
+            todos: [],
+            todo: ""
+        }
+    },
+    created() {
+        this.loadLocalStorage();
+    },
+    watch: {
+        todos() {
+            localStorage.setItem("todos", Json.stringify(this.todos));
+        }
+    },
+    methods: {
+        loadLocalStorage() {
+            const ls = JSON.parse(localStrorage.getItem("todos"));
+            console.log(ls);
+            if(ls == null) {return; }
+            this.todos = ls;
+            console.log(this.todos);
+        },
+        addTodo() {
+            this.todos.push(this.todo);
+            this.todo = "";
+        },
+        deleteTodo(index) {
+            this.todo.splice(index, 1);
+        }
+    }
+})
